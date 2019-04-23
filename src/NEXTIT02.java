c80044be55214088c99f642607b85732674202cc
HyeonTiger <49827360+HyeonTiger@users.noreply.github.com>
2019-04-23 21:31:53 +0900

    Create NEXTIT02.java

---
+++ src/NEXTIT02.java
@@ -0,0 +1,37 @@
+public class NEXTIT02 {
+public static void main (String[] args) {
+	/*
+	이름(문자)(name) String "홍길동";
+	나이(정수)(age) int 25;
+	성별(문자)(gender) char '남';
+	신장(실수)(height) double 180.3;
+	몸무게(실수)(weight) double 80.2;
+	결혼여부(판별)(married) boolean false;
+	전화번호(문자)(phone) String "010-1234-5678";
+	주소(문자)(address) String "환상의 나라"
+	*/
+	
+	String name = "홍길동";
+	int age = 25;
+	char gender = '남';
+	double height = 180.3;
+	double weight = 80.2;
+	boolean married = false;
+	String phone = "010-1234-5678";
+	String address = "환상의 나라";
+	
+	System.out.println("이름 : " + name + "\n나이 : " + age + "\n성별 : " + gender + "\n신장 : " + height + "\n몸무게 : " + weight + "\n결혼여부 : " + married + "\n전화번호 : " + phone + "\n주소 : " + address );
+	/*
+	 System.out.println("이름 : " + name);
+	 System.out.println("나이 : " + age);
+	 System.out.println("성별 : " + gender);
+	 System.out.println("신장 : " + height);
+	 System.out.println("몸무게 : " + weight);
+	 System.out.println("결혼여부 : " + married);
+	 System.out.println("전화번호 : " + phone);
+	 System.out.println("주소 : " + address);
+	 */
+
+	
+}
+}
